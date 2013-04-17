9f06f90a3016d14c5eca219fb40916396306b29e
Scott Bartell <me@scottbartell.com>
2013-04-17 04:31:17 -0400

    configure unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+working_directory "/home/deployer/apps/sb_production/current"
+stderr_path "/home/deployer/apps/sb_production/current/log/unicorn.log"
+pid "/home/deployer/apps/sb_production/current/tmp/pids/unicorn.pid"
+
+listen "/tmp/unicorn_sb_production.sock"
+worker_processes 1
