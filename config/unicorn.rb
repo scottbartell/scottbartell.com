873aad8c188d4bc7731c71ab28ee0d936936a2e8
Scott Bartell <me@scottbartell.com>
2013-04-24 03:55:50 -0400

    fix typo

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ working_directory "/home/deployer/apps/sb_production/current"
 stderr_path "/home/deployer/apps/sb_production/current/log/unicorn.log"
 pid "/home/deployer/apps/sb_production/current/tmp/pids/unicorn.pid"
 
-preload_app: true
+preload_app true
 listen "/tmp/unicorn_sb_production.sock"
 worker_processes 1
